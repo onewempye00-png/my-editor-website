c075600845fe1c1e2cfecfcbf61cf88fc7910e41
Speedy <onewempye00@gmail.com>
2026-05-03 19:08:03 +0200

    Refactor Firebase Admin initialization code

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -6,18 +6,3 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-};
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
-
-module.exports = db;
-module.exports = admin.firestore();
