1285edcef5c6b898ce96265b7052b4a404e54822
Speedy <onewempye00@gmail.com>
2026-05-04 16:17:46 +0200

    Update firebaseAdmin.js

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,18 +1,13 @@
 const admin = require("firebase-admin");
 
-if (!process.env.FIREBASE_PRIVATE_KEY) {
-    throw new Error("❌ FIREBASE_PRIVATE_KEY missing");
-}
-
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
     }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+
+    databaseURL: "https://my-editor-website-default-rtdb.europe-west1.firebasedatabase.app"
 });
 
-const db = admin.database(); // 🔥 IMPORTANT CHANGE
-
-module.exports = db;
+module.exports = admin.firestore();
